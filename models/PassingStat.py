e2287bfb31860f3c1df90f2c86a230772a08c07d
zachdenardi <zdenardi@gmail.com>
2026-07-02 07:08:07 -0700

    chore:fixed init import

--- models/PassingStat.py
+++ models/PassingStat.py
@@ -2,8 +2,7 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from db import Base
-from models.helper_classes import StatMixin
+from models.helper_classes import StatMixin, Base
 
 if TYPE_CHECKING:
     from models.Game import Game
