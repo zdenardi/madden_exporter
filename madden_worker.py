145c28d93eaa4031ce35c3936d788ef386eb683f
zachdenardi <zdenardi@gmail.com>
2026-07-27 23:31:35 -0700

    chore:dockerized madden worker

--- madden_worker.py
+++ madden_worker.py
@@ -4,9 +4,12 @@ import requests
 
 from dotenv import load_dotenv
 
+from services.slack_service import send_message
+
 load_dotenv()
 SYNC_ENDPOINT = os.getenv("SYNC_ENDPOINT")
 SYNC_INTERVAL = int(os.getenv("SYNC_INTERVAL", "3600"))
+send_message("Starting Worker...")
 while True:
     try:
         print("Syncing...")
